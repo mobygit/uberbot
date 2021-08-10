54c11a4eb0956d539964644c9a38581b708e0ed0
kieran keith <thekckfactor@gmail.com>
2021-08-10 18:36:40 -0400

    Some updates to index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const Commando = require('discord.js-commando');
+const Discord = require('discord.js');
+const path = require('path');
+
+const sqlite = require('sqlite');
+const sqlite3 = require('sqlite3');
+
+const client = new Commando.Client({
+	owner: '158327940311023616',
+	prefix: '!:
+});
+
+client.registry
+    // Registers your custom command groups
+    	.registerGroups([
+        	['fun', 'Fun commands'],
+		['util', 'Utility commands'],
+        	['commands', 'Uncategorized commands'],
+		['mod', 'Moderator commands']
+    	])
+	
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.setProvider(
+    sqlite.open({ filename: 'database.db', driver: sqlite3.Database }).then(db => new Commando.SQLiteProvider(db))
+).catch(console.error);
+
+client.login(process.env.TOKEN);
+
