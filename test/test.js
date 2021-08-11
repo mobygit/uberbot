762cedf4c749c5047ed55aa748ff6ae66c23e285
kieran keith <thekckfactor@gmail.com>
2021-08-11 15:50:10 -0500

    yasss

--- test/test.js
+++ test/test.js
@@ -1,7 +1,47 @@
-describe('Discord', function() {
-	it('should be able to successfully authenticate', function() {
-		var Discord = require('discord.js');
-		let client = new Discord.Client();
-		client.login('MzU0MzU4MDA3Mzc2NjQyMDQ4.Wa2znA.lIQZrJSSBMs655GHTTKen25jsmM');
+const Discord = require('discord.js-commando');
+
+const sqlite = require('sqlite');
+const sqlite3 = require('sqlite3');
+
+const Koa = require('koa');
+const _ = require('koa-route');
+
+const client = new Discord.Client({
+	owner = process.env.OWNER_ID,
+	prefix: '!'
+});
+
+describe('Discord', function () {
+	it('should be able to register defaults', function () {
+		client.registry
+			.registerDefaults();
+			
+	});
+	it('should be able to register commands'), function () {
+		client.registry
+			.registerCommandsIn(path.join(__dirname, '../commands'))
+	});
+	it('should be able to load the database', function () {
+		
+	});
+	it('should be able to successfully authenticate', function () {
+		client
+			.login(process.env.TOKEN);
+	});
+});
+
+var app;
+
+describe('Webserver', function() {
+	it('should be able to create an app', function() {
+		app = new Koa();
+	});
+
+	it('should be able to route', function() {
+		app.use(_.get('/', 'This is a test server.');
+	});
+
+	it('should be able to listen', function() {
+		app.listen(process.env.PORT || 8080);
 	});
 });
