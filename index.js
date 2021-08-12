c26ddaaee249d0d57345aad7e2adfd917e8f720c
kieran keith <thekckfactor@gmail.com>
2021-08-11 21:26:08 -0500

    ii8ii

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ client.registry
 	// .registerTypesIn(path.join(__dirname, 'types'));
 
 client
+	.on('error', console.error)
+	.on('warn', console.warn)
+	.on('debug', console.log)
+	
 	.on('ready', () => {
 		console.log('ready!');
 		client.user.setStatus('available');
@@ -34,6 +38,38 @@ client
 			type: 'STREAMING',
 			url: 'https://uberbot.xyz'
 		});
+	})
+	on('disconnect', () => { console.warn('Disconnected!'); })
+	.on('reconnecting', () => { console.warn('Reconnecting...'); })
+	.on('commandError', (cmd, err) => {
+		if(err instanceof commando.FriendlyError) return;
+		console.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
+	})
+	.on('commandBlocked', (msg, reason) => {
+		console.log(oneLine`
+			Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ''}
+			blocked; ${reason}
+		`);
+	})
+	.on('commandPrefixChange', (guild, prefix) => {
+		console.log(oneLine`
+			Prefix ${prefix === '' ? 'removed' : `changed to ${prefix || 'the default'}`}
+			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+		`);
+	})
+	.on('commandStatusChange', (guild, command, enabled) => {
+		console.log(oneLine`
+			Command ${command.groupID}:${command.memberName}
+			${enabled ? 'enabled' : 'disabled'}
+			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+		`);
+	})
+	.on('groupStatusChange', (guild, group, enabled) => {
+		console.log(oneLine`
+			Group ${group.id}
+			${enabled ? 'enabled' : 'disabled'}
+			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+		`);
 	});
 
 client.setProvider(
